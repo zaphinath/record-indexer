9b169a33fa789e94bde4bf2840ece755d252bde4
Derek Carr <zaphinath@gmail.com>
2013-07-10 09:39:24 -0600

    Starting the xml Parser

--- src/utils/Importer.java
+++ src/utils/Importer.java
@@ -7,7 +7,7 @@ public class Importer{
 
 
   public static void main(String[] args) {
-    
+    Parser parse = new Parser(); 
   
   }
 
--- src/utils/Parser.java
+++ src/utils/Parser.java
@@ -1,5 +1,12 @@
 package utils;
 
+import java.io.File;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.DocumentBuilder;
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
+import org.w3c.dom.Node;
+import org.w3c.dom.Element;
 
 public class Parser{
 
@@ -8,6 +15,22 @@ public class Parser{
 
   }
 
+  public void parse(File xml) {
+    try {
+      DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+      DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+      Document doc = dBuilder.parse(xml);
   
+      doc.getDocumentElement().normalize();
+
+      NodeList users = doc.getElementsByTagName("users");
+    
+
+
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  
+  }
 
 }
