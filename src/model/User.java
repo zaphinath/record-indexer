db868fcaef4265e94236dd186e0f5946fdd5faaf
Derek Carr <zaphinath@gmail.com>
2013-07-09 22:35:14 -0600

    Working on javadoc

--- src/model/User.java
+++ src/model/User.java
@@ -2,12 +2,26 @@ package model;
 
 public class User {
 
+  private String nameFirst;
+
 
 /*
  * Class constructor
  */
   User() {
 
+  }
+  /*
+   * @param name First Name
+   */
+  public void setFirstName(String name) {
+    nameFirst = name;
+  }
+  /*
+   * @return this.nameFirst
+   */
+  public String getFirstName() {
+    return this.nameFirst;
   }
 
 
