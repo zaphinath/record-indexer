9e0b3d092589822caaf7855770527619f867fa40
Derek Carr <zaphinath@gmail.com>
2013-07-15 22:35:51 -0600

    Project DOA and Project Unit Tests implemented

--- src/server/database/ProjectDB.java
+++ src/server/database/ProjectDB.java
@@ -1,5 +1,9 @@
 package server.database;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -12,12 +16,14 @@ import shared.model.Project;
  */
 public class ProjectDB {
 
+	private Database db;
+	
 	/**
 	 * Class Constructor
 	 * @param db
 	 */
 	public ProjectDB(Database db) {
-		
+		this.db = db;
 	}
 	
 	/**
@@ -25,35 +31,128 @@ public class ProjectDB {
 	 * @return List of all projects
 	 * 
 	 * @throws ServerException
+	 * @throws SQLException 
 	 */
-	public List<Project> getAll() throws ServerException {
+	public List<Project> getAll() throws ServerException, SQLException {
 		ArrayList<Project> projectList = new ArrayList<Project>();
-		
+		PreparedStatement stmt = null;
+    ResultSet rs = null;
+    try {
+    	String sql = "SELECT * FROM projects";
+    	stmt = db.getConnection().prepareStatement(sql);
+    	
+    	rs = stmt.executeQuery();
+    	while (rs.next()) {
+    		int id = rs.getInt(1);
+    		String title = rs.getString(2);
+    		int recordsPerImage = rs.getInt(3);
+    		int firstYCoord = rs.getInt(4);
+    		int recordHeight = rs.getInt(5);
+    		Project project = new Project(id, title, recordsPerImage, firstYCoord, recordHeight);
+    		projectList.add(project);
+    	}
+    } catch (SQLException e) {
+    	
+    } finally {
+    	if (rs != null) rs.close();
+    	if (stmt != null) stmt.close();
+    }
 		return projectList;
 	}
 	
 	/**
 	 * This adds a project to the database
-	 * @param project
+	 * @param project Project object
+	 * @return Project with id returned from the insertion
+	 * 
+	 * @throws ServerException
+	 * @throws SQLException 
 	 */
-	public void add(Project project) throws ServerException {
-		
+	public Project add(Project project) throws ServerException, SQLException {
+	  PreparedStatement stmt = null;
+	  Statement keyStmt = null;
+	  ResultSet keyRS = null;
+	  Project returnProject = null;
+	  try {
+	    String sql = "INSERT INTO projects (title, records_per_image, first_y_coord, record_height) VALUES(?, ?, ?, ?)";
+	    stmt = db.getConnection().prepareStatement(sql);
+	    stmt.setString(1, project.getTitle());
+	    stmt.setInt(2, project.getRecordsPerImage());
+	    stmt.setInt(3, project.getFirstYCoord());
+	    stmt.setInt(4, project.getRecordHeight());
+	    if (stmt.executeUpdate() == 1) {
+	    	keyStmt = db.getConnection().createStatement();
+	    	keyRS = keyStmt.executeQuery("SELECT last_insert_rowid()");
+	    	keyRS.next();
+	    	int projectId = keyRS.getInt(1);
+	    	returnProject = new Project(projectId, project.getTitle(), project.getRecordsPerImage(), project.getFirstYCoord(), project.getRecordHeight());
+	    } else {
+	      System.out.println("Insert user failed");
+	    }
+	  } catch (SQLException e) {
+	    e.printStackTrace();
+	  } finally {
+	    if (stmt != null) stmt.close();
+	    if (keyRS != null) keyRS.close();
+	    if (keyStmt != null) keyStmt.close();
+	  }
+	  return returnProject;
 	}
 	
 	/**
 	 * This updates a project in the database
 	 * @param project
+	 * @throws SQLException 
 	 */
-	public void update(Project project) throws ServerException {
-		
+	public void update(Project project) throws ServerException, SQLException {
+	  PreparedStatement stmt = null;
+	  Statement keyStmt = null;
+	  ResultSet keyRS = null;
+	  try {
+		  String sql = "UPDATE projects SET title = ?, records_per_image = ?, first_y_coord = ?, record_height = ? where id = ?";
+		  stmt = db.getConnection().prepareStatement(sql);
+		  stmt.setString(1, project.getTitle());
+		  stmt.setInt(2, project.getRecordsPerImage());
+		  stmt.setInt(3, project.getFirstYCoord());
+		  stmt.setInt(4, project.getRecordHeight());
+		  stmt.setInt(5, project.getId());
+		  if (stmt.executeUpdate() == 1) {
+			  System.out.println("SUCCESS");
+		  } else {
+			  System.out.println("UPDATE failed");
+		  }
+	  } catch (SQLException e) { e.printStackTrace(); }
+	  finally {
+		  if (stmt != null) stmt.close();
+		  if (keyRS != null) keyRS.close();
+		  if (keyStmt != null) keyStmt.close();
+	  }
 	}
 	
 	/**
 	 * This deletes a project from the database
 	 * @param project
 	 * @throws ServerException
+	 * @throws SQLException 
 	 */
-	public void delete(Project project) throws ServerException {
-		
+	public void delete(Project project) throws ServerException, SQLException {
+		PreparedStatement stmt = null;
+		ResultSet rs = null;
+		try {
+			String sql = "DELETE FROM projects WHERE id = ?";
+			stmt = db.getConnection().prepareStatement(sql);
+			stmt.setInt(1, project.getId());
+			
+			if (stmt.executeUpdate() == 1) {
+				//Success
+			} else {
+				//Fail
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			if (stmt != null) stmt.close();
+	  if (rs != null) rs.close();
+		}
 	}
 }
---
+++ test/server/database/ProjectDBTest.java
@@ -0,0 +1,161 @@
+/**
+ * 
+ */
+package server.database;
+
+import static org.junit.Assert.*;
+import shared.model.Project;
+import server.ServerException;
+import server.database.*;
+
+import java.sql.SQLException;
+import java.util.List;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ * @author zaphinath
+ *
+ */
+public class ProjectDBTest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+		Database.initialize();
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+		return;
+	}
+
+	private Database db;
+	private ProjectDB  projectDb;
+	
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+		db = new Database("indexer_server_test.db");
+		db.startTransaction();
+		projectDb = db.getProjectDB();
+		List<Project> projects = projectDb.getAll();
+		
+		for (Project project : projects) {
+			db.getProjectDB().delete(project);
+		}
+		
+		db.endTransaction(true);
+		
+		db = new Database("indexer_server_test.db");
+		db.startTransaction();
+		projectDb = db.getProjectDB();
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+		db.endTransaction(false);
+		
+		db = null;
+		projectDb = null;
+	}
+
+	@Test
+	public void testAddProject() throws ServerException, SQLException {
+		Project project1 = new Project(-1, "title", 20, 0, 800);
+		Project project2 = new Project(-1, "facePlant", 13, 10, 600);
+		
+		project1 = projectDb.add(project1);
+		project2 = projectDb.add(project2);
+		
+		List<Project> all = projectDb.getAll();
+		assertEquals(2,all.size());
+		
+		assertEquals(project1.getId(), all.get(0).getId());
+		assertEquals(project1.getTitle(), all.get(0).getTitle());
+		assertEquals(project1.getRecordsPerImage(), all.get(0).getRecordsPerImage());
+		assertEquals(project1.getFirstYCoord(), all.get(0).getFirstYCoord());
+		assertEquals(project1.getRecordHeight(), all.get(0).getRecordHeight());
+		
+		assertEquals(project2.getId(), all.get(1).getId());
+		assertEquals(project2.getTitle(), all.get(1).getTitle());
+		assertEquals(project2.getRecordsPerImage(), all.get(1).getRecordsPerImage());
+		assertEquals(project2.getFirstYCoord(), all.get(1).getFirstYCoord());
+		assertEquals(project2.getRecordHeight(), all.get(1).getRecordHeight());
+	}
+
+	@Test
+	public void testUpdateProject() throws ServerException, SQLException {
+		Project project1 = new Project(-1, "title", 20, 0, 800);
+		Project project2 = new Project(-1, "facePlant", 13, 10, 600);
+		
+		project1 = projectDb.add(project1);
+		project2 = projectDb.add(project2);
+		
+		List<Project> all = projectDb.getAll();
+		assertEquals(2,all.size());
+		
+		project1.setTitle("Holy");
+		project1.setRecordsPerImage(2);
+		project1.setFirstYCoord(10);
+		project1.setRecordHeight(1600);
+		
+		project2.setTitle("Bible");
+		project2.setRecordsPerImage(0);
+		project2.setFirstYCoord(100);
+		project2.setRecordHeight(800);
+		
+		projectDb.update(project1);
+		projectDb.update(project2);
+		
+		all = projectDb.getAll();
+		
+		assertEquals(project1.getId(), all.get(0).getId());
+		assertEquals(project1.getTitle(), all.get(0).getTitle());
+		assertEquals(project1.getRecordsPerImage(), all.get(0).getRecordsPerImage());
+		assertEquals(project1.getFirstYCoord(), all.get(0).getFirstYCoord());
+		assertEquals(project1.getRecordHeight(), all.get(0).getRecordHeight());
+		
+		assertEquals(project2.getId(), all.get(1).getId());
+		assertEquals(project2.getTitle(), all.get(1).getTitle());
+		assertEquals(project2.getRecordsPerImage(), all.get(1).getRecordsPerImage());
+		assertEquals(project2.getFirstYCoord(), all.get(1).getFirstYCoord());
+		assertEquals(project2.getRecordHeight(), all.get(1).getRecordHeight());
+		
+		all = projectDb.getAll();
+		assertEquals(2,all.size());
+	}
+	
+	@Test
+	public void testDeleteProject() throws ServerException, SQLException {
+		Project project1 = new Project(-1, "title", 20, 0, 800);
+		Project project2 = new Project(-1, "facePlant", 13, 10, 600);
+		
+		project1 = projectDb.add(project1);
+		project2 = projectDb.add(project2);
+		
+		List<Project> all = projectDb.getAll();
+		assertEquals(2,all.size());
+		
+		for (Project project : all) {
+			db.getProjectDB().delete(project);
+		}
+		
+		all = projectDb.getAll();
+		assertEquals(0,all.size());
+	}
+}
