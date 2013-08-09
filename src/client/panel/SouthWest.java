b096946319d8dbde544a110445ceb1625988d5c6
Derek Carr <zaphinath@gmail.com>
2013-08-09 17:38:37 -0600

    added if catch for array out of bounds exception

--- src/client/panel/SouthWest.java
+++ src/client/panel/SouthWest.java
@@ -43,12 +43,14 @@ public class SouthWest extends JPanel {
 				Component comp = tabs.getSelectedComponent();
 				if (comp.equals(fec)) {
 					//TODO fix this
-					fec.getValues().get(session.getSelectedCell().getField()-1).requestFocusInWindow();
-					for (int i = 0; i < session.getNumRecords(); i++) {
-						if (i == session.getSelectedCell().getRecord()) {
-							for (int j = 1; j < session.getNumFields(); j++) {
-								fec.getValues().get(j-1).setText(session.getValue(j, i));
-				   		 	}
+					if (session.getSelectedCell().getField() > 0) {
+						fec.getValues().get(session.getSelectedCell().getField()-1).requestFocusInWindow();
+						for (int i = 0; i < session.getNumRecords(); i++) {
+							if (i == session.getSelectedCell().getRecord()) {
+								for (int j = 1; j < session.getNumFields(); j++) {
+									fec.getValues().get(j-1).setText(session.getValue(j, i));
+					   		 	}
+							}
 						}
 					}
 				}
